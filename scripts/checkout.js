fec309c8ec27f9c789546b38ea807c163bb0d666
Devil_Alert <devidasdeo9@gmail.com>
2024-01-08 21:08:09 +0530

    Making delete link interactive.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js"
+import { cart, removeFromCart } from "../data/cart.js"
 import { products } from "../data/products.js"
 import { formatCurrency } from "./utils/money.js";
 
@@ -15,7 +15,7 @@ cart.forEach((cartItem,index) => {
     })
 
     let htmlString = 
-        `<div class="cart-item-container">
+        `<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
             Delivery date: Tuesday, June 21
             </div>
@@ -38,7 +38,7 @@ cart.forEach((cartItem,index) => {
                     <span class="update-quantity-link link-primary">
                         Update
                     </span>
-                    <span class="delete-quantity-link link-primary">
+                    <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                         Delete
                     </span>
                 </div>
@@ -95,3 +95,15 @@ cart.forEach((cartItem,index) => {
 })
 
 document.querySelector(".js-order-summary").innerHTML = totalHTML;
+
+
+document.querySelectorAll(".js-delete-link")
+    .forEach((link) => {
+        link.addEventListener("click",() => {
+            const productId = link.dataset.productId;
+            removeFromCart(productId);
+            
+            const container = document.querySelector(`.js-cart-item-container-${productId}`)
+            container.remove();
+        })
+    })
